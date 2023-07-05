51c05145db3b5973803df4972158674f67c743fb
David Corbacho Roman <david@dolittle.com>
2023-07-06 02:59:56 +0300

    Polish it, new bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
-
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -16,6 +16,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        id="reveal-items"
+        dangerouslySetInnerHTML={{
+          __html: `
+            const elements = document.querySelectorAll('.shwoop-in');
+            let i = 0;
+            elements.forEach((element) => {
+              element.style.setProperty('--i', i);
+              i++;
+            });
+
+            const elements2 = document.querySelectorAll('.shwoop-bottom');
+            let j = 8;
+            elements2.forEach((element) => {
+              element.style.setProperty('--j', j);
+              j++;
+            })
+          `,
+        }}
+      />
       <body className={inter.className}>
         {children}
         <Analytics />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,106 @@
 import Image from "next/image";
-import { Merriweather } from "next/font/google";
+import { Space_Mono } from "next/font/google";
 
-const mainFont = Merriweather({ weight: ["400", "700"], subsets: ["latin"] });
+const mainFont = Space_Mono({ weight: ["400", "700"], subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="bodybg">
-      <Image
-        src="/bg.svg"
-        alt="Background image"
-        className="object-cover object-center dark:invert transform tw-scale-x-1"
-        fill
-        priority
-        style={{ transform: "scaleX(-1)" }}
-      />
-
+    <main className={`${mainFont.className} `}>
+      <div className="extraShadow flex min-h-screen flex-col p-12 ">
+        <Image
+          src="/background.png"
+          alt="Background image"
+          className="object-cover object-center dark:invert transform tw-scale-x-1"
+          fill
+          priority
+        />
+      </div>
       <div className="flex min-h-screen flex-col p-12 ">
         <h1
-          className={`${mainFont.className} mb-3 text-6xl font-700 text-white z-20`}
+          className={`${mainFont.className} mb-3 text-5xl font-700 text-white z-20`}
         >
           David Corbacho Román
         </h1>
-        <p
-          className={`${mainFont.className} mb-3 text-xl font-400 text-white z-20`}
-        >
-          <br />
-          「　UX Engineer　」
-          <br />
-          <br /> ⭐️ Motivated by helping others, speaker
-          <br /> 🇪🇸 Spanish expat in Finland 🇫🇮
-          <br /> 🇧🇻{" "}
-          <a
-            href="https://nordlys.studio"
-            target="_blank"
-            rel="noopener noreferrer"
+        <br />
+        <div>
+          <span className={`uxEngineer text-2xl font-semibold text-white`}>
+            UX Engineer
+          </span>
+          <br></br>
+          <span
+            className={`uxEngineer digital text-xl font-400 font-bold text-white`}
           >
-            nordlys.studio
-          </a>{" "}
-          • happy remote
-          <br /> 🟦 dcor.bsky.social
-          <br /> 🐦 @dcorbacho
-        </p>
+            DIGITAL
+          </span>
+        </div>
+        <div className={` bulletPoints mb-3 text-xl font-400 text-white z-20`}>
+          <br />
+          <br />
+          <div className="shwoop-in"></div>
+          <div className="shwoop-in"> ⭐️ He knows his stuff</div>
+          <div className="shwoop-in">
+            🎩 Motivated by helping others, speaker
+          </div>
+          <div className="shwoop-in"> 🇪🇸 Spanish expat in Finland 🇫🇮</div>
+          <div className="shwoop-in">
+            🇧🇻{" "}
+            <a
+              href="https://nordlys.studio"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              nordlys.studio co-founder
+            </a>{" "}
+            • remote
+          </div>
+        </div>
+
+        <footer className={`mb-3 text-l font-400 text-white z-20`}>
+          <div className="shwoop-bottom">
+            |
+            <a
+              href="https://bsky.app/profile/dcor.dev"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="shwoop-bottom"
+            >
+              Bluesky
+            </a>
+          </div>{" "}
+          <div className="shwoop-bottom">
+            |
+            <a
+              href="https://twitter.com/dcorbacho"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="shwoop-bottom"
+            >
+              Twitter
+            </a>
+          </div>{" "}
+          <div className="shwoop-bottom">
+            |
+            <a
+              href="https://www.linkedin.com/in/corbacho/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="shwoop-bottom"
+            >
+              LinkedIn
+            </a>
+          </div>{" "}
+          <div className="shwoop-bottom">
+            |
+            <a
+              href="https://github.com/dcorb"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="shwoop-bottom"
+            >
+              Github
+            </a>
+          </div>
+        </footer>
       </div>
     </main>
   );
